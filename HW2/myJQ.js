2ea552666f6552aad2301f5826ce3888d1c5e25f
Nathan Stark <nstark13@wou.edu>
2017-10-21 21:23:52 -0700

    working on the div for okay, next work on my css.

--- HW2/myJQ.js
+++ HW2/myJQ.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+	//I've set my variable to stop undefined type coming across
 	var nam="";
 	$("#sayHello").click(function(){
 		nam = $("#textBox1").val();
@@ -16,6 +17,9 @@ $(document).ready(function(){
 			$("#inputBox").hide();
 		}
 		});
+	//here I had some troble working with my buttons that I added in the above function
+	//but after some research I came across the .on() which bond the click functional that
+	//I needed for the DOM to be properly altered. 
 	$("#welcome").on('click',"#good",function() {
 		$("#goodHit").html("<h3>I'm so happy for you!</h3>" + 
 		"<p>Here is a quote to help you along your way.</p>" + 
@@ -32,7 +36,8 @@ $(document).ready(function(){
 	});
 	
 	$("#welcome").on('click',"#bad",function() {
-		$("#badHit").html("it worked not bad");
+		$("#badHit").html("I'm sorry to hear that. Maybe you should try this " + 
+		" <a href='https://www.youtube.com/watch?v=ekm0BsI2qfY'>video</a>.");
 		
 		$("#welcome").hide();
 		

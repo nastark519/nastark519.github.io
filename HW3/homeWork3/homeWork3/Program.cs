d481176ba13c1ffb3f11006ef38e93b790c92caa
Nathan Stark <nstark13@wou.edu>
2017-10-26 22:40:04 -0700

    I have finished my main method in my Calculator class and am now working on my DoCalc methon and EvaluatePostFixInput method.

--- HW3/homeWork3/homeWork3/Program.cs
+++ HW3/homeWork3/homeWork3/Program.cs
@@ -6,10 +6,63 @@ using System.Threading.Tasks;
 
 namespace homeWork3
 {
-    class Program
+    class Calculator
     {
+
         static void Main(string[] args)
         {
+            Calculator app = new Calculator();
+            bool playAgain = true;
+            Console.WriteLine("\nPostfix Calculator. Recognizes these operators: + - * /");
+
+            while (playAgain)
+            {
+                playAgain = app.DoCalculation();
+            }
+            Console.WriteLine("SEE YA.");
+            /*int i;
+            bool isVal = int.TryParse(Console.ReadLine(),out i);
+            if (isVal)
+            {
+                Console.WriteLine(">>" + i + "\n");
+            }*/
+        }
+
+        private bool DoCalculation()
+        {
+            Console.WriteLine("Please enter q to quit\n");
+            string sInput = "2 2 +";
+            Console.WriteLine(">");
+
+            sInput = Console.ReadLine();
+
+            if (sInput[0] == 'q' || sInput[0] == 'Q')
+            {
+                return false;
+            }
+            string sOutput = "4";
+            /*since the next step involes a try catch block
+             * envolving evaluatePostFixInput() method 
+             * I am going to move on to making that methon
+             * in my C# code*/
+
+            /*
+            bool isVal1 = int.TryParse(Console.ReadLine(), out input1);
+            if (!isVal1)
+            {
+                return false;
+            }
+            */
+            return true;
+        }
+
+        public string EvaluatePostFixInput(string input)
+        {
+            if (input == null || input == "")
+            {
+
+            }
+            return "";
         }
     }
 }

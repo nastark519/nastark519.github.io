cc8ea6e966c1d19652ae2923a73e3ce5e1e305a8
Nathan Stark <nstark13@wou.edu>
2017-10-30 20:31:00 -0700

    Working now on what should be the last part of the Calculator. Working in the method DoCalculation.

--- HW3/homeWork3/homeWork3/Program.cs
+++ HW3/homeWork3/homeWork3/Program.cs
@@ -111,18 +111,45 @@ namespace homeWork3
                         throw new ArgumentException("Improper input format. Stack became empty when expecting first operand.");
                     }
                     a = (Convert.ToDouble(stack.Pop()));
-                    //c = DoOperation(a, b, s);
+                    c = DoOperation(a, b, s);
+                    stack.Push(Convert.ToDouble(c));
                 }
-            }
+            }//end foreach
 
 
-            return "";
+            return Convert.ToString(stack.Pop());
         }
 
         public double DoOperation(double a, double b, string s)
         {
+            double c = 0.0;
+            if (s == "+")
+                c = ( a + b );
+            else if (s == "-")
+                c = ( a - b );
+            else if (s == "*")
+                c = ( a * b );
+            else if(s == "/")
+            {
+                try
+                {
+                    c = (a / b);
+                    if(c == double.NegativeInfinity || c == double.PositiveInfinity)
+                    {
+                        throw new ArgumentException("Can't divide by zero");
+                    }
+                }
+                catch (ArithmeticException e)
+                {
+                    throw new ArgumentException(e.Message);
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Improper operator: " + s + ", is not one of +, -, *, or /");
+            }
 
-            return 0.0;
+            return c;
         }
     }
 

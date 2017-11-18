6050b7caaae4f15b4b3a647e38403d3cb3f0d92d
Nathan Stark <nstark13@wou.edu>
2017-11-17 20:39:27 -0800

    Working on step 5 on to model class

---
+++ HW5/HW5/DAL/DMVContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HW5.DAL
+{
+    public class DMVContext : DbContext
+    {
+        public DMVContext() : base("name=DbStuff") { }
+
+        public virtual DbSet<DmvStuff> DmvStuff { get; set; }
+    }
+}

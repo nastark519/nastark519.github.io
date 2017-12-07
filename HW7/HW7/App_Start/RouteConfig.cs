86fc41a1d1c860e8d4641b114ea9881ad07636dd
Nathan Stark <nstark13@wou.edu>
2017-12-06 21:15:08 -0800

    time to think having a brain fart.

--- HW7/HW7/App_Start/RouteConfig.cs
+++ HW7/HW7/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace HW7
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
                 name: "Search",
                 url: "search",
